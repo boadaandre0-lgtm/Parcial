3d5c5656b76978c56a3678e98f6afa9fc1d1c2a4
Estudiante <uestudiantes@uniempresarial.edu.co>
2026-08-13 08:16:50 -0500

    Creacion de assets y se intento agregar la animacion de movimiento

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var speed = 200.0
 @export var gravity = 900
 @export var jump_force = -400
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	var dir = Input.get_axis("move_left", "move_right")
@@ -13,4 +14,10 @@ func _physics_process(delta):
 	if  Input.is_action_just_pressed("jump"):
 		velocity.y = jump_force
 
-# TODO: falta la función de salto
+func update_animation(direction):
+	if direction == Vector2.ZERO:
+		animated_sprite.play("idle")
+	else:
+		animated_sprite.play("walk")
+	if direction.x != 0:
+		animated_sprite.flip_h = direction.x < 0
